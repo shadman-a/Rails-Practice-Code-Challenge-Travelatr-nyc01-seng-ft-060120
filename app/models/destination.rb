0e6acf55f56c4e840325f80a151efbe614a2e057
shadman-a <shadman@outlook.com>
2020-06-29 18:52:13 -0400

    Done.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,11 +2,11 @@ class Destination < ApplicationRecord
     has_many :posts
     has_many :bloggers, through: :posts 
 
-    def most_reacent
+    def most_recent
         self.posts.last(5)
     end
 
-    def feactured 
+    def featured 
         self.posts.max_by {|post| post.likes}
     end
 end
